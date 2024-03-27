1d6dfb963edb170d7d64f51e0a869db2b04ff822
Christopher Dearie <cdearie@gmail.com>
2024-03-27 21:27:46 +0000

    More updates

--- provider.tf
+++ provider.tf
@@ -1,25 +1,16 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+  backend "s3" {
+    bucket = "{{terraform_state_bucket}}"
+    key    = "{{terraform_state_key}}"
+    region = "us-west-2"
   }
 }
 
 provider "aws" {
-  assume_role {
-    role_arn     = "arn:aws:iam::123456789012:role/ROLE_NAME"
-    session_name = "SESSION_NAME"
-    external_id  = "EXTERNAL_ID"
-  }
+
 }
 
 provider "aws" {
   alias = "root"
-  assume_role {
-    role_arn      = ""
-    session_name  = ""
-    external_id   = ""
-  }
+
 }
