37edd1d365bc15e34b0573ff91e1ccb48f89eb4a
Christopher Dearie <cdearie@gmail.com>
2024-03-30 00:12:43 +0000

    gg

--- aws_lambda_account_resolver.tf
+++ aws_lambda_account_resolver.tf
@@ -13,6 +13,7 @@ data "aws_iam_policy_document" "lambda_assume_role" {
 
 resource "aws_iam_role" "account_resolver_lambda" {
   name               = "SSO-Assigner-Account-Resolver-Lambda"
+  path               = "/app/"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
 }
 
